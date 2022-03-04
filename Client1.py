dc8d3a021f80fc6984ae8f471b85dbe4dcdabfb8
tathaiduy <69078392+tathaiduy@users.noreply.github.com>
2022-03-04 17:56:50 +0700

    Update Client1.py

--- Client1.py
+++ Client1.py
@@ -6,7 +6,7 @@ from typing import Tuple
 
 class Client():
 	run=False
-	def __init__(self, connect:Tuple[str,int]=("1.52.38.20",9999)) -> None:
+	def __init__(self, connect:Tuple[str,int]=("127.0.0.1",9999)) -> None:
 		signal.signal(signal.SIGINT, self.exit_gracefully)
 		signal.signal(signal.SIGTERM, self.exit_gracefully)
 		self.stop = False
